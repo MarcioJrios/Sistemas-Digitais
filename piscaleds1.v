5b5b2d63eea9cda0fcfc9a96ca047965801d9324
Vitor Antonio <31110983+vitor-apolinario@users.noreply.github.com>
2017-09-12 14:29:42 -0300

    Update piscaleds1.v

--- piscaleds1.v
+++ piscaleds1.v
@@ -1,4 +1,4 @@
-module piscaleds1(
+module piscaleds(
 input CLOCK_50, 
 input [3:0] KEY, 
 input [9:0] SW,
@@ -9,24 +9,31 @@ output [7:0]LEDR
 //assign LEDG = KEY;
 //assign LEDR = SW;
 
-reg [25:0] contador = 0;
+reg [27:0] contador = 0;
 reg l = 0;
 assign LEDG = l;
+assign LEDR = SW[0] * SW[1];
 
 always @(posedge CLOCK_50) begin
 	contador = contador + 1;
-	if(KEY[0] == 1) begin
+	if(SW[0] == 0 && SW[1] == 0) begin
 		if(contador == 50000000) begin
 		contador = 0;
 		l = ~l;
 		end
 	end
-	if(KEY[0] == 0) begin
-		if(contador == 5000000) begin
+	if(SW[0] ==0 && SW[1]==1) begin
+		if(contador == 90000000)begin
 		contador = 0;
 		l = ~l; 
 		end
+	end
+	if(SW[0] == 1 && SW[1]==0) begin
+		if(contador == 25000000)begin
+		contador = 0;
+		l = ~l; 
 		end
+	end
 end
 
 

80bf28b2fe7ed1c8b530f8867d19a8db8b4967f4
MarcioJrios <31110984+MarcioJrios@users.noreply.github.com>
2017-09-19 15:38:44 -0300

    Add files via upload

---
+++ triangulo.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+typedef struct{
+	int x, y;
+}ponto;
+
+int main(){
+	ponto p1;
+	ponto p2;
+	ponto p3;
+	ponto p;
+	int alpha;
+	int beta;
+	int gamma;
+	int i;
+	int j;
+	p1.x = 12;
+	p1.y = 5;
+	p2.x = 5;
+	p2.y = 16;
+	p3.x = 2;
+	p3.y = 2;
+	for(i = 0; i < 20; i++){
+		for(j = 0; j < 20; j++){
+		p.x = i;
+		p.y = j;
+		if((p.x == p1.x && p.y == p1.y) || (p.x == p2.x && p.y == p2.y) || (p.x == p3.x && p.y == p3.y))
+			printf("*");
+		else{
+			alpha = ((p2.y - p3.y)*(p.x - p3.x) + (p3.x - p2.x)*(p.y - p3.y)) /
+        		((p2.y - p3.y)*(p1.x - p3.x) + (p3.x - p2.x)*(p1.y - p3.y));
+			beta = ((p3.y - p1.y)*(p.x - p3.x) + (p1.x - p3.x)*(p.y - p3.y)) /
+       			((p2.y - p3.y)*(p1.x - p3.x) + (p3.x - p2.x)*(p1.y - p3.y));
+			gamma = 1 - alpha - beta;
+			if(alpha > 0 && beta > 0 && gamma > 0)
+				printf("* ");
+			else
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+}

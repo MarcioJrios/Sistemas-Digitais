b1d21e323e0eeec4b5ae30139c4d329f1c9dc44b
MarcioJrios <31110984+MarcioJrios@users.noreply.github.com>
2017-10-02 15:49:43 -0300

    Update trianguloFinal.c

--- trianguloFinal.c
+++ trianguloFinal.c
@@ -4,9 +4,9 @@ typedef struct{
 	int x, y;
 }ponto;
 
-	//|a.x  a.y 1|a.x  a.y
-	//|b.x  b.y 1|b.x  b.y 
-	//|c.x  c.y 1|c.x  c.y
+	//|a.x  a.y 1|a.x  a.y           a=(12,5)
+	//|b.x  b.y 1|b.x  b.y 		 b=(5,16)
+	//|c.x  c.y 1|c.x  c.y		 c=(2,2)
 int calculaArea(ponto a, ponto b, ponto c){
 	int A;
 	A = ((a.x * b.y) + (a.y * c.x) + (b.x * c.y)) - ((a.y * b.x) + (a.x * c.y) + (b.y * c.x));
@@ -29,7 +29,7 @@ int main(){
 	int area = calculaArea(a, b, c);
 	if(area < 0)
 		area = -area;
-		printf("%d\n", area);
+	printf("%d\n", area);
 	int AB, AC, BC;
 	for(i = 0; i < 20; i++){
 		for(j = 0; j < 20; j++){
@@ -40,11 +40,11 @@ int main(){
 			AB = -AB;
 		//printf("AB:%d   \n", AB);
 		AC = calculaArea(a, c, p);
-		if(AB < 0)
+		if(AC < 0)
 			AC = -AC;
 		//printf("AC:%d   \n", AC);
 		BC = calculaArea(b, c, p);
-		if(AB < 0)
+		if(BC < 0)
 			BC = -BC;
 		//printf("BC:%d   \n", BC);
 		if(AB + AC + BC == area)

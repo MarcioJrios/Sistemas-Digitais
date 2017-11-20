10e451a2f3d2474b0a1b7eeda21568545b59b431
MarcioJrios <31110984+MarcioJrios@users.noreply.github.com>
2017-11-20 10:12:46 -0200

    Update triangulo.v

--- triangulo.v
+++ triangulo.v
@@ -1,51 +1,31 @@
-module piscaleds(
-input CLOCK_50, 
-output S,
+module triangulo(
+   input CLOCK_50,
+   input [8:0] ax,
+   input [8:0] bx,
+   input [8:0] cx,
+   input [8:0] ay,
+   input [8:0] by,
+   input [8:0] cy,
+   output S
 );
 
-
-reg [8:0] ax = 12;
-reg [8:0] ay = 5;
-reg [8:0] bx = 5;
-reg [8:0] by = 16;
-reg [8:0] cx = 2;
-reg [8:0] cy = 2;
 reg [8:0] px = 0;
 reg [8:0] py = 0;
-reg [8:0] area = 0;
 reg [8:0] AB = 0;
 reg [8:0] AC = 0;
 reg [8:0] BC = 0;
 
-area T(..., AT);
-area t1(..., at1);
-area t2(..., at2);
-area t3(..., at3;
+area T(ax, bx, cx, ay, by, cy, AT);
 
-assign S = AT == at1 + at2 + a3;
-
-initial begin
-	A = ((ax * by) + (ay * cx) + (bx * cy)) - ((ay * bx) + (ax * cy) + (by * cx));
-	if(area < 0) begin
-		area = -area;
-	end
-end
 
 always @(posedge CLOCK_50) begin
+	#dumpvars(S);
 	if(py < 20) begin
-		AB = ((ax * by) + (ay * px) + (bx * py)) - ((ay * bx) + (ax * py) + (by * px));
-		if(AB < 0) begin
-			AB = -AB;
+		area ab(ax, bx, px, ay, by, py, TAB);
+		area ac(ax, px, cx, ay, py, cy, TAC);
+		area bc(px, bx, cx, py, by, cy, TBC);
 		end
-		AC = ((ax * py) + (ay * cx) + (px * cy)) - ((ay * px) + (ax * cy) + (py * cx));
-		if(AC < 0) begin
-			AC = -AC;
-		end
-		BC = ((px * by) + (py * cx) + (bx * cy)) - ((py * bx) + (px * cy) + (by * cx));
-		if(BC < 0) begin
-			BC = -BC;
-		end
-	if(AB + AC + BC == area) begin
+	if(TAB + TAC + TBC == AT) begin
 		S = 1;
 	end else begin
 		S = 0;

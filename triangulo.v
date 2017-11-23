32d8a08bc01102d95e79f8f1d3def02d9b6bc04d
MarcioJrios <31110984+MarcioJrios@users.noreply.github.com>
2017-11-23 17:35:24 -0200

    Update triangulo.v

--- triangulo.v
+++ triangulo.v
@@ -1,5 +1,5 @@
 module triangulo(
-   input CLOCK_50,
+   input clk,
    input [8:0] ax,
    input [8:0] bx,
    input [8:0] cx,
@@ -9,34 +9,15 @@ module triangulo(
    output S
 );
 
-reg [8:0] px = 0;
-reg [8:0] py = 0;
-reg [8:0] AB = 0;
-reg [8:0] AC = 0;
-reg [8:0] BC = 0;
+reg [8:0] TAB = 0;
+reg [8:0] TAC = 0;
+reg [8:0] TBC = 0;
 
 area T(ax, bx, cx, ay, by, cy, AT);
 
-
-always @(posedge CLOCK_50) begin
-	#dumpvars(S);
-	if(py < 20) begin
-		area ab(ax, bx, px, ay, by, py, TAB);
-		area ac(ax, px, cx, ay, py, cy, TAC);
-		area bc(px, bx, cx, py, by, cy, TBC);
-		end
-	if(TAB + TAC + TBC == AT) begin
-		S = 1;
-	end else begin
-		S = 0;
-	end
-	
-	py = py+1;
-	end
-		else begin
-			px = px+1;
-			py = 0;
-		end
-end
+area ab(ax, bx, px, ay, by, py, TAB);
+area ac(ax, px, cx, ay, py, cy, TAC);
+area bc(px, bx, cx, py, by, cy, TBC);
+assign S = TAB + TAC + TBC == AT;
 
 endmodule

fe719506f758498a37ada32fc9bc60c39355bec4
MarcioJrios <31110984+MarcioJrios@users.noreply.github.com>
2017-11-30 15:22:56 -0200

    Update area.v

--- area.v
+++ area.v
@@ -5,11 +5,12 @@ module area(
    input [8:0] ay,
    input [8:0] by,
    input [8:0] cy,
-   output [8:0] Area
+   output [17:0] Area
 );
-
+	reg [17:0] A;
+	assign Area = A;
 always @( ax or bx or cx or ay or by or cy ) begin
-	Area = ((ax * by) + (ay * cx) + (bx * cy)) - ((ay * bx) + (ax * cy) + (by * cx));
+	A = ((ax * by) + (ay * cx) + (bx * cy)) - ((ay * bx) + (ax * cy) + (by * cx));
 end
 // 
 

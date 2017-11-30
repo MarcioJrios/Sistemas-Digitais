bd19492eba035c35614ef4b299940fd1b4214e7d
MarcioJrios <31110984+MarcioJrios@users.noreply.github.com>
2017-11-30 15:23:32 -0200

    Update triangulo.v

--- triangulo.v
+++ triangulo.v
@@ -8,16 +8,17 @@ module triangulo(
    input [8:0] cy,
    output S
 );
-
-reg [8:0] TAB = 0;
-reg [8:0] TAC = 0;
-reg [8:0] TBC = 0;
+reg [17:0] AT = 0;
+reg [17:0] TAB = 0;
+reg [17:0] TAC = 0;
+reg [17:0] TBC = 0;
 
 area T(ax, bx, cx, ay, by, cy, AT);
 
 area ab(ax, bx, px, ay, by, py, TAB);
 area ac(ax, px, cx, ay, py, cy, TAC);
 area bc(px, bx, cx, py, by, cy, TBC);
-assign S = TAB + TAC + TBC == AT;
+
+assign S = (TAB + TAC + TBC == AT) ? 1 : 0;
 
 endmodule
